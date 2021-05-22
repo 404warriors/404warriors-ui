774334bdc7cb3319016308bc8545a1cdbcebdc71
Michael Timmons <michael.timmons@protonmail.com>
2021-05-21 20:34:12 -0400

    Clean Up

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Amplify from 'aws-amplify';
-import { withAuthenticator, SignUpConfig, SignInProps, UsernameAttribute } from 'amplify-material-ui';
+import { withAuthenticator, SignUpConfig, UsernameAttribute } from 'amplify-material-ui';
 
 import awsconfig from './aws-exports';
 import logo from './logo.svg';
