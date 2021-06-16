9b03e9d17b627296c34bf4e415861a683b2ae7f3
Michael Timmons <michael.timmons@protonmail.com>
2021-06-15 21:09:57 -0400

    Add Link to Register Page

--- src/views/Landing/components/BootcampPromo/Promo.tsx
+++ src/views/Landing/components/BootcampPromo/Promo.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import { Typography, Button } from '@material-ui/core';
-import { SectionHeader } from 'components/molecules';
+import React from "react";
+import { Typography, Button } from "@material-ui/core";
+import { SectionHeader } from "components/molecules";
+import { Link } from "react-router-dom";
 
-const PromoNumbers = ({ data, className, ...rest }: ViewComponentProps): JSX.Element => {
+const PromoNumbers = ({
+  data,
+  className,
+  ...rest
+}: ViewComponentProps): JSX.Element => {
   return (
     <div className={className} {...rest}>
       <SectionHeader
         title={
           <span>
-            The first coding bootcamp is coming to {' '}
-            <Typography component="span" variant="inherit" color="primary">Fort Bragg</Typography>
+            The first coding bootcamp is coming to{" "}
+            <Typography component="span" variant="inherit" color="primary">
+              Fort Bragg
+            </Typography>
           </span>
         }
         subtitle="Become a
@@ -17,8 +24,18 @@ const PromoNumbers = ({ data, className, ...rest }: ViewComponentProps): JSX.Ele
         in 12 or 24 Weeks."
         fadeUp
         ctaGroup={[
-          <Button variant="outlined" color="primary" size="large">Learn More</Button>,
-          <Button variant="contained" color="primary" size="large">Apply now</Button>
+          <Button variant="outlined" color="primary" size="large">
+            Learn More
+          </Button>,
+          <Link
+            to={{
+              pathname: "/register",
+            }}
+          >
+            <Button variant="contained" color="primary" size="large">
+              Apply now
+            </Button>
+          </Link>,
         ]}
       />
     </div>
